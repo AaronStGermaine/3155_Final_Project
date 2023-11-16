7c547f2663676b72e9e3c44d57af0d5062a7f7e3
spichay <spichay@uncc.edu>
2023-11-16 16:55:47 -0500

    Fix Routers

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -1,8 +1,8 @@
 class conf:
-    db_host = "localhost"
+    db_host = "127.0.0.1"
     db_name = "sandwich_maker_api"
     db_port = 3306
     db_user = "root"
-    db_password = "rootroot"
+    db_password = "password"
     app_host = "localhost"
     app_port = 8000
--- api/routers/index.py
+++ api/routers/index.py
@@ -1,11 +1,12 @@
-from . import orders, order_details, customers, ratings, recipes, resources, foods
+from . import orders, promos, order_details, customers, ratings, recipes, resources, foods
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
-    app.include_router(customers)
-    app.include_router(ratings)
-    app.include_router(recipes)
-    app.include_router(resources)
-    app.include_router(sandwiches)
+    app.include_router(customers.router)
+    app.include_router(ratings.router)
+    app.include_router(recipes.router)
+    app.include_router(resources.router)
+    app.include_router(foods.router)
+    app.include_router(promos.router)
