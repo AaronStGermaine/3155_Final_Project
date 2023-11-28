d9758c439e628694dc39eba014378b1b40b032fa
Trevor Richardson <Tjrich336@gmail.com>
2023-11-28 18:37:50 -0500

    Fix: Updated temp db_password.

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -3,6 +3,6 @@ class conf:
     db_name = "sandwich_maker_api"
     db_port = 3306
     db_user = "root"
-    db_password = "Helicopter22!"
+    db_password = "rootroot"
     app_host = "localhost"
     app_port = 8000
